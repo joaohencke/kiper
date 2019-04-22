3f33ea08b4a03503b68dd96b426b1b44962cb908
Joao Hencke <joao.hencke@gmail.com>
2019-04-22 16:55:04 -0300

    resident tests

---
+++ ___specs___/resident/index.spec.js
@@ -0,0 +1,49 @@
+const MongoMemory = require('mongodb-memory-server').default;
+const mongoose = require('mongoose');
+const { expect } = require('chai');
+const manager = require('../../resident');
+
+let mongod;
+
+describe('resident tests', () => {
+  before(async () => {
+    mongod = new MongoMemory({ binary: { version: '4.0.0' } });
+    return mongoose.connect(await mongod.getConnectionString(), {
+      useNewUrlParser: true,
+    });
+  });
+
+  describe('manager', () => {
+    it('should create a resident', async () => {
+      const resident = await manager.create({
+        name: 'Ygor', email: 'ygor@kiper.com.br', apartment: 101, block: 'A',
+      });
+
+      expect(resident).to.have.property('_id');
+      return true;
+    });
+
+    it('should create a resident with cpf', async () => {
+      const resident = await manager.create({
+        name: 'Ygor', email: 'ygor@kiper.com.br', apartment: 101, block: 'A', cpf: '04678444963',
+      });
+
+      expect(resident).to.have.property('_id');
+      expect(resident).to.have.property('cpf');
+
+      return true;
+    });
+
+    it('should throw error because of invalid block', async () => {
+      try {
+        const resident = await manager.create({
+          name: 'Ygor', email: 'ygor@kiper.com.br', apartment: 101, block: 'E', cpf: '04678444963',
+        });
+        return resident;
+      } catch (e) {
+        expect(e.message).to.contains('not a valid enum value for path `block`');
+        return true;
+      }
+    });
+  });
+});
